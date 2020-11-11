3dfc58ad0fed640ac874a0a22994ffa5e117100e
guoyuqi1 <74292670+guoyuqi1@users.noreply.github.com>
2020-11-11 17:04:40 +0800

    Create 输出.c

---
+++ 输出.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int i;
+int arr[ ]={31,28,31,30,31,30,31,31,30,31,30,31};
+for(i=1;i<=12;i++)
+{
+printf("%d\n",arr[i]);
+}
+scanf("%d",&i);
+printf("%d\n",arr[i-1]);
+return 0;
+}
