77c7e84a4b4e1e56b4a7e6d56d75eed84d0507d6
guoyuqi1 <74292670+guoyuqi1@users.noreply.github.com>
2020-11-16 20:28:13 +0800

    Create 七大罪.c

---
+++ 七大罪.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    scanf("%d",&i);
+if(i>0 && i<=1)
+    {
+    printf("傲慢之罪\n");
+    }
+else if(i>1 && i<=2)
+    {
+    printf("嫉妒之罪/n");
+    }
+else if(i>2 && i<=3)
+    {
+    printf("愤怒之罪\n");
+    }
+    else if(i>3 && i<=4)
+    {
+    printf("怠惰之罪\n");
+    }
+    else if(i>4 && i<=5)
+    {
+    printf("贪婪之罪\n");
+    }
+    else if(i>5 && i<=6)
+    {
+    printf("暴食之罪\n");
+    }
+    else if(i>6 && i<=7)
+    {
+    printf("色欲之罪\n");
+    }
+    return 0;
+}
