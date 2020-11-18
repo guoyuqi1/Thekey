ec42c8cebe9bee610d2fcca4e2cbff8efaded2e5
guoyuqi1 <74292670+guoyuqi1@users.noreply.github.com>
2020-11-18 09:02:58 +0800

    Create 20.11.18.作业.c

---
+++ 20.11.18.作业.c
@@ -0,0 +1,87 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	a=3;
+	b=4;
+	c=5;
+	if (a+b>c && b==c)
+	{
+	printf("真命题");
+	}
+	else
+	{
+	printf("假命题");
+	}
+	return 0;
+}
+
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	a=3;
+	b=4;
+	c=5;
+	if (a||b+c && b-c)
+	{
+	printf("真命题");
+	}
+	else
+	{
+	printf("假命题");
+	}
+	return 0;
+}
+
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	a=3;
+	b=4;
+	c=5;
+	if (!(a>b) && !c||1)
+	{
+	printf("真命题");
+	}
+	else
+	{
+	printf("假命题");
+	}
+	return 0;
+}
+
+#include<stdio.h>
+int main()
+{
+	int a,b,c,x,y;
+	a=3,b=4,c=5; 
+	x=a,y=b;
+	if (!(x=a) && (y=b) && 0)
+	{
+	printf("真命题");
+	}
+	else
+	{
+	printf("假命题");
+	}
+	return 0;
+}
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	a=3;
+	b=4;
+	c=5;
+	if (!(a+b)+c-1 && b+c/2)
+	{
+	printf("真命题");
+	}
+	else
+	{
+	printf("假命题");
+	}
+	return 0;
+}
