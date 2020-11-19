573865be3d041cb9d13f583e7024b843bcee6c98
guoyuqi1 <74292670+guoyuqi1@users.noreply.github.com>
2020-11-19 19:47:03 +0800

    Create 111803.作业.c

---
+++ 111803.作业.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+int i;
+i=1 && 0;
+scanf("%d",&i);
+if(i==0)
+{
+printf("女");
+}
+else if(i==1)
+{
+printf("男");
+}
+return 0;
+}
+
+#include<stdio.h>
+int main()
+{
+	printf("\n    ★\n");
+	printf("   ★★\n");
+	printf("  ★★★\n");
+	printf(" ★★★★\n");
+	printf("★★★★★\n");
+return 0;
+}
+
