af0470ced5fac25451594ca6d1bc97129554a99d
guoyuqi1 <74292670+guoyuqi1@users.noreply.github.com>
2020-11-25 09:47:14 +0800

    世界大学排名

---
+++ 112502.QS排名查询.app.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    scanf("%d",&i);
+    if(i==1)
+        {
+        printf("麻省理工学院");
+        }
+    else if(i==2)
+        {
+        printf("斯坦福大学");
+        }
+        else if(i==3)
+        {
+        printf("哈佛大学");
+        }
+        else if(i==4)
+        {
+        printf("牛津大学");
+        }
+                else if(i==5)
+        {
+        printf("加州理工学院");
+        }
+                else if(i==6)
+        {
+        printf("苏黎世联邦理工大学");
+        }
+                else if(i==7)
+        {
+        printf("剑桥大学");
+        }
+                else if(i==8)
+        {
+        printf("伦敦大学学院");
+        }
+                else if(i==9)
+        {
+        printf("帝国理工学院");
+        }
+                else if(i==10)
+        {
+        printf("芝加哥大学");
+        }
+        else
+            {
+            printf("你不配");
+            }
+        return 0;
+}
