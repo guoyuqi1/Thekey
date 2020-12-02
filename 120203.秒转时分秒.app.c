08ff7f64b5fb0cc71d62d022fa7d7e21caaeef16
guoyuqi1 <74292670+guoyuqi1@users.noreply.github.com>
2020-12-02 16:50:10 +0800

    输入秒数，转换成时分秒

---
+++ 120203.秒转时分秒.app.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main()
+{
+    int i,a,b,c,d;
+    printf("欢迎使用死兆星软件！\n");
+    printf("请输入秒数:\n");
+    scanf("%d",&i);
+        a=i/3600;
+        b=i/60;
+        c=i%60;
+        if(a<10)
+            {
+            printf("0%d:",a);
+            }
+        else
+            {
+            printf("%d:",a);
+            }
+        if(b<10)
+            {
+            printf("0%d:",b);
+            }
+        else
+            {
+            printf("%d:",b);
+            }
+        if(c<10)
+            {
+            printf("0%d:",c);
+            }
+        else
+            {
+            printf("%d:",c);
+            }
+    return 0;
+}
