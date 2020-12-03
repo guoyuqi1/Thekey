013dda88638f4543a26626ce4fce2e143dd5a705
guoyuqi1 <74292670+guoyuqi1@users.noreply.github.com>
2020-12-03 20:57:45 +0800

    一维数字

---
+++ 120205.一维.app.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    int arr[]={250,38,1314,520};
+    int array2d[2][3]=
+    {
+        {34324,23434,564},
+        {46234,24234,525},
+    };
+    for(i=0;i<4;i++)
+        {
+        printf("%d ",arr[i]);
+        }
+    return 0;
+}
