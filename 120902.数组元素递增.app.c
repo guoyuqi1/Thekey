8a86885d434b6eed897f88941f6c29f666716779
guoyuqi1 <74292670+guoyuqi1@users.noreply.github.com>
2020-12-09 09:41:20 +0800

    Create 120902.数组元素递增.app.c

---
+++ 120902.数组元素递增.app.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int row,column;
+	int arr[9][10]={0};
+	for(row=0;row<9;row++){
+		for(column=0;column<10;column++){
+			printf("%d ",arr[row][column]);}
+	printf("\n");
+	}
+	for(row=0;row<9;row++){
+		for(column=0;column<10;column++){
+			arr[row][column]=(row)*10+(column)+1;
+			printf("%-2d ",arr[row][column]);}
+	printf("\n");
+	}
+	return 0;
+}
