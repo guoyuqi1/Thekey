c6ea9cdb663834ef07d63bc8b0e58f0bae614490
guoyuqi1 <74292670+guoyuqi1@users.noreply.github.com>
2020-12-09 17:04:38 +0800

    Create 120905.串长度.app.c

---
+++ 120905.串长度.app.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    printf("欢迎使用TX软件！\n");
+    char s1[]="DNF";
+    char s2[]="英雄联盟";
+    printf("%d",sizeof(s1));
+    printf("\n");
+    printf("%d",sizeof(s2));
+    return 0;
+}
