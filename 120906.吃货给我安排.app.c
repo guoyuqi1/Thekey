2e663cee0b9f4c1247f9987ac9bab38ae38e48c1
guoyuqi1 <74292670+guoyuqi1@users.noreply.github.com>
2020-12-10 20:59:40 +0800

    Update 120906.吃货给我安排.app.c

--- 120906.吃货给我安排.app.c
+++ 120906.吃货给我安排.app.c
@@ -6,16 +6,16 @@ int main()
     printf("请输入节日\n");
     char s1[50];
     scanf("%s",&s1);
-    if(strcmp(s1,"春节"))
+    if(！strcmp(s1,"春节"))
         {
             printf("给我安排饺子\n");
         }
-        else if(strcmp(s1,"端午节"))
+        else if(！strcmp(s1,"端午节"))
             {
             printf("给我安排饺子\n");
             }
-        else if(strcmp(s1,"中秋节")){printf("给我安排月饼\n");}
-        else if(strcmp(s1,"元宵节")){printf("给我安排元宵\n");}
+        else if(！strcmp(s1,"中秋节")){printf("给我安排月饼\n");}
+        else if(！strcmp(s1,"元宵节")){printf("给我安排元宵\n");}
         else{printf("暂无安排");}
     return 0;
 }
